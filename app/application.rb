f753d08d0de1ba3cf0da917ee7ce66052ee5cf21
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-07-08 04:49:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
         item_name = req.path.split("/items/").last 
         item = @@items.detect{|i| i.name = item_name}
         
-      if item.nil
+      if item i = nil
         resp.write "Item Not Found"
         resp.status = 400
       else
