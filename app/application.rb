54975dca67cd2d61552dbd5cf41568e1878df0d4
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-07-08 04:23:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,20 +1,7 @@
 class Application
   
-  @@Items[]
+  @@Items = []
   
   def call(env)
     
-    resp = Rack::Response.new 
-    rep = Rack::Request.new(env)
-    
-    if req.path=="/items"
-      resp.write "#{price}"
-    else
-    resp.write "Route not found"
-    resp.status 404
-  end
-end
-  
-  resp.finish
-  
-end
+   
