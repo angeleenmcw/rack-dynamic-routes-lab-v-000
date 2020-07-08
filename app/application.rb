9c1761a2eff36173a9080d11e0e042481b7df446
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-07-08 04:50:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,28 +2,25 @@ class Application
   
   @@Items = []
   
-  def call(env)
-    
+     def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
-      
+
       if req.path.match(/items/)
-        item_name = req.path.split("/items/").last 
-        item = @@items.detect{|i| i.name = item_name}
-        
-      if item i = nil
-        resp.write "Item Not Found"
-        resp.status = 400
+        item_name = req.path.split("/items/").last
+        item = @@items.find{|i| i.name == item_name}
+        if item != nil
+          resp.write item.price
+        else
+          resp.write "Item not found"
+          resp.status = 400
+        end
       else
-        resp.write item.price 
+        resp.write "Route not found"
+        resp.status = 404
       end
-      
-    else resp.write "Route not Found"
-      resp.status = 404 
+
+      resp.finish
     end
-    
-    resp.finish
-  end
+
 end
-        
-        
